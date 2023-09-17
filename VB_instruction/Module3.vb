4e9ce31bf9fbd1e9c6be93d3acbc0bfa9878695f
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-09-17 18:20:54 +0900

    66

---
+++ VB_instruction/Module3.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Runtime.Serialization.Formatters
+
+Module Module3
+
+    'コメント
+
+    Sub Main()
+        Dim I As Integer
+        Dim J As Integer
+        Console.WriteLine("マルチステートメント")
+        I = 0 : J = 1
+
+        If I = 0 And
+            J = 2 Then
+            Console.WriteLine("ステートメント")
+        End If
+
+        If I = 0 And
+           J = 1 Then
+            Console.WriteLine("")
+        End If
+
+        '入力値を取得
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ VB_instruction/My Project/Module2 (2).vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.Serialization.Formatters
+
+Module Module2
+
+    Sub Main()
+        Dim I As Integer
+        Dim J As Integer
+        Console.WriteLine("マルチステートメント")
+        I = 0 : J = 1
+
+        If I = 0 And
+            J = 2 Then
+            Console.WriteLine("ステートメント")
+        End If
+
+        If I = 0 And
+           J = 1 Then
+            Console.WriteLine("")
+        End If
+
+        '入力値を取得
+        Console.ReadLine()
+
+    End Sub
+
+End Module
